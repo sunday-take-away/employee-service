07e7f824b4e1da3b70be86b9e75c7e9be656a380
Sunday Take Away <eugene.le.roux@icloud.com>
2018-07-31 13:54:48 +0200

    wired in employee http end-point

--- src/main/scala/com/takeaway/service/employee/http/HttpRoutes.scala
+++ src/main/scala/com/takeaway/service/employee/http/HttpRoutes.scala
@@ -3,9 +3,10 @@ package com.takeaway.service.employee.http
 import akka.http.scaladsl.server.Directives._
 import com.takeaway.service.employee.http.api.doc.SwaggerDocService
 import com.takeaway.service.employee.http.header.CORSHeaderHandler
+import com.takeaway.service.employee.http.route.EmployeeRoute
 
-trait HttpRoutes extends HttpRouteBase with CORSHeaderHandler {
+trait HttpRoutes extends HttpRouteBase with CORSHeaderHandler with EmployeeRoute {
 
-  allHttpRoutes = corsHandler(allHttpRoutes ~ SwaggerDocService.routes)
+  allHttpRoutes = corsHandler(allHttpRoutes ~ employeeRoutes ~ SwaggerDocService.routes)
 
 }
